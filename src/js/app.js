a4efdf57eced7cc5599e94ffffacbd08b62c061d
Michael Andrew <michael@gladeye.co.nz>
2016-12-22 15:23:49 +1300

    Added initial teleport control support

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-text-component';
+import 'aframe-teleport-controls';
 import 'babel-polyfill';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
@@ -26,12 +27,18 @@ class VRScene extends React.Component {
   render () {
     return (
       <Scene>
+
+          <Entity teleport-controls vive-controls="hand: left" />
+
         <Camera>
           <a-cursor
             animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
 >
           </a-cursor>
+
         </Camera>
+
+
 
         <Sky src="url(https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg)"/>
 
@@ -55,6 +62,19 @@ class VRScene extends React.Component {
             animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
             geometry='primitive: box; depth: 0.2; height: 0.2; width: 0.2'
             material={{color: '#24CAFF'}}/>
+        </Entity>
+
+        <Entity
+            animation__rot={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
+            animation__sca={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+            geometry='primitive: box'
+            material={{color: this.state.color, opacity: 0.6}}
+            position='0 1 10'
+            onClick={this.changeColor.bind(this)}>
+            <Entity
+                animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+                geometry='primitive: box; depth: 0.2; height: 0.2; width: 0.2'
+                material={{color: '#24CAFF'}}/>
         </Entity>
       </Scene>
     );
