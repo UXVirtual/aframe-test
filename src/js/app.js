065aa98e45e3550990742ec1aee37e115d1d1b4e
Michael Andrew <michael@gladeye.co.nz>
2016-12-23 13:12:14 +1300

    Testing adding audio

--- src/js/app.js
+++ src/js/app.js
@@ -25,37 +25,32 @@ class VRScene extends React.Component {
     return (
       <Scene vr-mode-ui="enabled: true">
 
-          <a-entity position="0 0.5 0" rotation="0 0 0">
-              <a-camera></a-camera>
-          </a-entity>
-
           <a-assets>
               <a-asset-item id="optimerBoldFont" src="#optimerBoldTypeface" />
               a-assets>
               <img id="grid" src="https://img.gs/bbdkhfbzkk/stretch/https://i.imgur.com/25P1geh.png" crossOrigin="anonymous"/>
               <img id="sky" src="https://img.gs/bbdkhfbzkk/2048x2048,stretch/http://i.imgur.com/WqlqEkq.jpg" crossOrigin="anonymous" />
+              <audio id="driveradio" src="http://streaming.radionomy.com/DRIVE:80" preload="auto" crossOrigin="anonymous" />
 
           </a-assets>
 
-          <Entity teleport-controls hand-controls="left" position="0 -999999999 0" />
+          <a-sound src="src: #driveradio" autoplay="true" position="0 1 10"></a-sound>
 
-          <Entity hand-controls="right" position="0 -999999999 0" />
+          <Entity bmfont-text="text: Hello world; color: white" position='-1.75 1 -3'></Entity>
 
-          <Entity bmfont-text="text: Hello world" position='-1.75 1 -3'></Entity>
-
-        <Camera>
-          <a-cursor
-            animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
->
-          </a-cursor>
-
-        </Camera>
-
+          <Entity position="0 1.8 4" rotation="0 0 0">
+              <Entity teleport-controls vive-controls="hand: left" />
+              <Entity vive-controls="hand: right" />
+            <Camera>
+              <a-cursor
+                animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150" />
+            </Camera>
+          </Entity>
 
 
         <a-sky src="#sky" rotation="0 -90 0"/>
 
-          <Entity position="0 0 0"
+          <Entity position="0 -1 0"
                     geometry="primitive: plane; width: 10000; height: 10000" rotation="-90 0 0"
                     material="src: #grid; repeat: 10000 10000; transparent: true;"/>
 
@@ -63,10 +58,7 @@ class VRScene extends React.Component {
           <Entity light="color: #ffaaff; intensity: 1.5;" position="5 5 5"/>
           <Entity light="color: white; intensity: 0.5;" position="-5 5 15"/>
           <Entity light="color: white; type: ambient;"/>
-
-  />
             <AnimatedCube position='0 2 -3' />
-
             <AnimatedCube position='0 1 10' />
       </Scene>
     );
