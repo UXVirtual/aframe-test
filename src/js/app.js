31d6ceeab8eb2739c46102175f29436ce6bb9882
Michael Andrew <michael@gladeye.co.nz>
2016-12-30 13:54:49 +1300

    Changes to scene
    Added test model, sounds and changed colors of textures / lights
    Removed animated cube
    Began adding physics to scene

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import 'aframe';
+import 'aframe-terrain-model-component';
 import 'aframe-animation-component';
 import 'aframe-teleport-controls';
 import 'aframe-bmfont-text-component';
+import 'aframe-physics-components';
 import 'babel-polyfill';
 import 'kframe';
 
@@ -20,46 +22,78 @@ class VRScene extends React.Component {
 
   render () {
 
-      var optimerBoldTypeface = require('three/examples/fonts/optimer_bold.typeface');
-
     return (
-      <Scene vr-mode-ui="enabled: true">
+      <Scene physics-world="gravity: 0 -9.8 0" vr-mode-ui="enabled: true">
 
           <a-assets>
-              <a-asset-item id="optimerBoldFont" src="#optimerBoldTypeface" />
-              a-assets>
-              <img id="grid" src="https://img.gs/bbdkhfbzkk/stretch/https://i.imgur.com/25P1geh.png" crossOrigin="anonymous"/>
-              <img id="sky" src="https://img.gs/bbdkhfbzkk/2048x2048,stretch/http://i.imgur.com/WqlqEkq.jpg" crossOrigin="anonymous" />
-              <audio id="driveradio" src="http://streaming.radionomy.com/DRIVE:80" preload="auto" crossOrigin="anonymous" />
+              <a-asset-item id="uxvirtual-outer-obj" src="assets/obj/uxvirtual-logo/uxvirtual-outer.obj"></a-asset-item>
+              <a-asset-item id="uxvirtual-outer-mtl" src="assets/obj/uxvirtual-logo/uxvirtual-outer.mtl"></a-asset-item>
+              <a-asset-item id="uxvirtual-inner-b-obj" src="assets/obj/uxvirtual-logo/uxvirtual-inner-b.obj"></a-asset-item>
+              <a-asset-item id="uxvirtual-inner-b-mtl" src="assets/obj/uxvirtual-logo/uxvirtual-inner-b.mtl"></a-asset-item>
+              <a-asset-item id="uxvirtual-inner-t-obj" src="assets/obj/uxvirtual-logo/uxvirtual-inner-t.obj"></a-asset-item>
+              <a-asset-item id="uxvirtual-inner-t-mtl" src="assets/obj/uxvirtual-logo/uxvirtual-inner-t.mtl"></a-asset-item>
 
+              <audio id="newbuntu-sound" src="assets/mp3/newbuntu.ogg" preload="auto" />
+                  <audio id="spaceambient-sound" src="assets/mp3/spaceambient.ogg" preload="auto" />
+                      <audio id="system-ready-sound" src="assets/mp3/system-ready.ogg" preload="auto" />
+              <img id="grid" src="assets/img/grid.png" crossOrigin="anonymous"/>
+              <img id="sky" src="assets/img/skybox2.jpg" crossOrigin="anonymous" />
           </a-assets>
 
-          <a-sound src="src: #driveradio" autoplay="true" position="0 1 10"></a-sound>
+          <Entity id="start-sound" position="0 0 -20" sound="src: #newbuntu-sound; autoplay: true; loop: false; volume: 10;"/>
 
-          <Entity bmfont-text="text: Hello world; color: white" position='-1.75 1 -3'></Entity>
+          <Entity id="logo" position="0 0 -20" sound="src: #spaceambient-sound; autoplay: true; loop: true; volume: 30">
+              <Entity obj-model="obj: #uxvirtual-outer-obj; mtl: #uxvirtual-outer-mtl" sound="src: #system-ready-sound; autoplay: false; loop: false; volume: 30; on: click">
+                  <a-animation attribute="rotation"
+                               dur="10000"
+                               fill="forwards"
+                               to="0 360 0"
+                               repeat="indefinite"></a-animation>
+              </Entity>
+              <Entity obj-model="obj: #uxvirtual-inner-b-obj; mtl: #uxvirtual-inner-b-mtl">
+                  <a-animation attribute="rotation"
+                               dur="10000"
+                               fill="forwards"
+                               to="0 360 0"
+                               delay="500"
+                               repeat="indefinite"></a-animation>
+              </Entity>
+              <Entity obj-model="obj: #uxvirtual-inner-t-obj; mtl: #uxvirtual-inner-t-mtl">
+                  <a-animation attribute="rotation"
+                               dur="10000"
+                               fill="forwards"
+                               to="0 360 0"
+                               delay="250"
+                               repeat="indefinite"></a-animation>
+              </Entity>
+          </Entity>
+
 
           <Entity position="0 1.8 4" rotation="0 0 0">
               <Entity teleport-controls vive-controls="hand: left" />
               <Entity vive-controls="hand: right" />
-            <Camera>
-              <a-cursor
-                animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150" />
-            </Camera>
+
+              <Camera id="camera">
+                  <a-cursor
+                  animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150" />
+              </Camera>
+
           </Entity>
 
+          <Entity id="terrain" position="0 -75 0" rotation="0 -90 0" terrain-model='color: #736357; roughness: 1; shading: flat; DEM: url(assets/obj/terrain/noctis-3500-clip-envi.bin); planeWidth: 346; planeHeight: 346; segmentsWidth: 199; segmentsHeight: 199; zPosition: 100;'></Entity>
 
-        <a-sky src="#sky" rotation="0 -90 0"/>
 
+          <a-sky src="#sky" rotation="0 -90 0"/>
           <Entity position="0 -1 0"
-                    geometry="primitive: plane; width: 10000; height: 10000" rotation="-90 0 0"
-                    material="src: #grid; repeat: 10000 10000; transparent: true;"/>
+                    geometry="primitive: box; width: 10000; height: 0.1; depth: 10000;" rotation="-90 0 0"
+                    material="src: #grid; repeat: 10000 10000; transparent: true;"
+                  physics-body="mass: 0; boundingBox: 10000 0.1 10000"
+              />
 
-          <Entity light="color: #ccccff; intensity: 1; type: ambient;" visible=""/>
-          <Entity light="color: #ffaaff; intensity: 1.5;" position="5 5 5"/>
-          <Entity light="color: white; intensity: 0.5;" position="-5 5 15"/>
-          <Entity light="color: white; type: ambient;"/>
-            <AnimatedCube position='0 2 -3' />
-            <AnimatedCube position='0 1 10' />
+          <Entity light="color: #736357;" position="-1 1 0"></Entity>
+          <Entity light="color: #ffb820; type: point;" position="0 5 0"></Entity>
+          <Entity light="color: #736357; type: ambient"></Entity>
+
       </Scene>
     );
   }
--- src/js/components/AnimatedCube.js
+++ src/js/components/AnimatedCube.js
@@ -20,15 +20,38 @@ export class AnimatedCube extends Component {
         });
     }
 
+    applyImpulse() {
+        var scene = document.querySelector('a-scene');
+        var el = scene.querySelector('#'+this.props.id);
+        var camera = scene.querySelector('#camera');
+
+        console.log('Camera',camera);
+
+        var matrixWorld = camera.object3D.matrixWorld;
+        var position = new THREE.Vector3();
+        position.setFromMatrixPosition(matrixWorld);
+
+        var hitMeBody = el.components['physics-body'];
+        //TODO figure out how to get vector coordinates based on direction that camera is facing (maybe need to do a raycast and get its vector?)
+        //see https://github.com/jesstelford/aframe-click-drag-component/blob/master/src/index.js for examples of doing this based on mouse cursor position
+        hitMeBody.applyImpulse(camera.object3D.rotation, position);
+    }
+
+    onClick() {
+        this.changeColor();
+        this.applyImpulse();
+    }
+
     render() {
         return (
-            <Entity
+            <Entity id={this.props.id}
                 animation__rot={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
                 animation__sca={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
                 geometry='primitive: box'
+                physics-body="boundingBox: 1 1 1; mass: 25; velocity: 0 0 0"
                 material={{color: this.state.color, opacity: 0.6}}
                 position={this.props.position}
-                onClick={this.changeColor.bind(this)}>
+                onClick={this.onClick.bind(this)}>
                 <Entity
                     animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
                     geometry='primitive: box; depth: 0.2; height: 0.2; width: 0.2'
