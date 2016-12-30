178f2a0c2a1cde8cde11086f09ce2528fbaf8edc
Michael Andrew <michael@gladeye.co.nz>
2016-12-30 15:34:18 +1300

    Now using aframe-extras controls

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import 'aframe';
+import 'aframe-extras';
 import 'aframe-terrain-model-component';
 import 'aframe-animation-component';
 import 'aframe-teleport-controls';
@@ -19,6 +20,13 @@ import Sky from './components/Sky';
 
 class VRScene extends React.Component {
 
+    constructor(props){
+        super(props);
+        var extras = require('aframe-extras');
+        extras.controls.registerAll();
+        extras.primitives.registerAll();
+    }
+
 
   render () {
 
@@ -32,6 +40,8 @@ class VRScene extends React.Component {
               <a-asset-item id="uxvirtual-inner-b-mtl" src="assets/obj/uxvirtual-logo/uxvirtual-inner-b.mtl"></a-asset-item>
               <a-asset-item id="uxvirtual-inner-t-obj" src="assets/obj/uxvirtual-logo/uxvirtual-inner-t.obj"></a-asset-item>
               <a-asset-item id="uxvirtual-inner-t-mtl" src="assets/obj/uxvirtual-logo/uxvirtual-inner-t.mtl"></a-asset-item>
+              <a-asset-item id="desert-tower-obj" src="assets/obj/desert-tower/desert-tower.obj"></a-asset-item>
+              <a-asset-item id="desert-tower-mtl" src="assets/obj/desert-tower/desert-tower.mtl"></a-asset-item>
 
               <audio id="newbuntu-sound" src="assets/mp3/newbuntu.ogg" preload="auto" />
                   <audio id="spaceambient-sound" src="assets/mp3/spaceambient.ogg" preload="auto" />
@@ -41,6 +51,13 @@ class VRScene extends React.Component {
           </a-assets>
 
           <Entity id="start-sound" position="0 0 -20" sound="src: #newbuntu-sound; autoplay: true; loop: false; volume: 10;"/>
+
+          <Entity id="desert-tower" position="-100.70 -25.47 37.63" rotation="2.82 45.00 4.50" scale="0.5 0.42 0.5" obj-model="obj: #desert-tower-obj; mtl: #desert-tower-mtl" />
+
+          <Entity id="desert-tower" position="29.65 -12.75 -74.01" rotation="0 18.00 -19.00" scale="0.7 0.68 0.7" obj-model="obj: #desert-tower-obj; mtl: #desert-tower-mtl" />
+
+
+          <Entity id="desert-tower" position="-54.7 20.72 -74.01" obj-model="obj: #desert-tower-obj; mtl: #desert-tower-mtl" />
 
           <Entity id="logo" position="0 0 -20" sound="src: #spaceambient-sound; autoplay: true; loop: true; volume: 30">
               <Entity obj-model="obj: #uxvirtual-outer-obj; mtl: #uxvirtual-outer-mtl" sound="src: #system-ready-sound; autoplay: false; loop: false; volume: 30; on: click">
@@ -68,17 +85,7 @@ class VRScene extends React.Component {
               </Entity>
           </Entity>
 
-
-          <Entity position="0 1.8 4" rotation="0 0 0">
-              <Entity teleport-controls vive-controls="hand: left" />
-              <Entity vive-controls="hand: right" />
-
-              <Camera id="camera">
-                  <a-cursor
-                  animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150" />
-              </Camera>
-
-          </Entity>
+              <Camera id="camera" universal-controls />
 
           <Entity id="terrain" position="0 -75 0" rotation="0 -90 0" terrain-model='color: #736357; roughness: 1; shading: flat; DEM: url(assets/obj/terrain/noctis-3500-clip-envi.bin); planeWidth: 346; planeHeight: 346; segmentsWidth: 199; segmentsHeight: 199; zPosition: 100;'></Entity>
 
@@ -91,8 +98,8 @@ class VRScene extends React.Component {
               />
 
           <Entity light="color: #736357;" position="-1 1 0"></Entity>
-          <Entity light="color: #ffb820; type: point;" position="0 5 0"></Entity>
-          <Entity light="color: #736357; type: ambient"></Entity>
+          <Entity id="point-light" light="color: #ffb820; type: point;" position="0 5 0"></Entity>
+          <Entity id="ambient-light" light="color: #736357; type: ambient"></Entity>
 
       </Scene>
     );
